77b98107d76685a9ccb56e541cf1107c5215b744
SEOYOUNG-cloud <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2022-02-20 22:33:38 +0900

    문자열 내 p와 y의 개수
    프로그래머스 문자열 내 p와 y의 개수 for JAVA

---
+++ programmers/PandY.java
@@ -0,0 +1,17 @@
+public class PandY { // 문자열 내 p와 y의 개수
+	boolean solution(String s) {
+        boolean answer = true;
+
+        int p = 0, y = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'P' || s.charAt(i) == 'p') p++;
+            else if(s.charAt(i) == 'Y' || s.charAt(i) == 'y') y++;
+        }
+        
+        if(p == y) answer = true;
+        else answer = false;
+
+        return answer;
+    }
+
+}
