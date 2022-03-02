cf426ec1c9839e1a110328dcfdcdf2a78f5969fe
SEOYOUNG-cloud <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2022-03-02 18:52:21 +0900

    CodeUp_6016~6020
    코드업 기초 100제 6016~6020

---
+++ CodeUp/CodeUp_6016.py
@@ -0,0 +1,19 @@
+# 6016
+a,b = input().split()
+print(b,a) # 쉼표 넣으면 공백 두고 출력됨
+
+# 6017
+s = input()
+print(s,s,s)
+
+# 6018
+a,b = input().split(':') # : 를 기준으로 넣음
+print(a,b,sep = ':') # sep을 사용하면 이 기호를 사이에 두고 값 출력
+
+# 6019
+a,b,c = input().split('.')
+print(c,b,a,sep = '-')
+
+# 6020
+a,b = input().split('-')
+print(a+b)
