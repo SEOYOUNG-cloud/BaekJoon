14d154cc4d30f0d67aecccf35322b0feb8817535
SEOYOUNG-cloud <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2022-03-29 20:21:56 +0900

    괄호 문자열의 적합성 판단.py
    [코드트리] 괄호 문자열의 적합성 판단.python

---
+++ CodeTree/parentheses_stack.py
@@ -0,0 +1,42 @@
+class Stack:
+    def __init__(self):          # 빈 스택 하나를 생성합니다.
+        self.items = []
+                
+    def push(self, item):        # 스택에 데이터를 추가합니다.
+        self.items.append(item)
+                
+    def empty(self):             # 스택이 비어있으면 True를 반환합니다.
+        return not self.items
+                
+    def size(self):              # 스택에 있는 데이터 수를 반환합니다.
+        return len(self.items)
+        
+    def pop(self):               # 스택의 가장 위에 있는 데이터를 반환하고 제거합니다.
+        if self.empty():
+            raise Exception("Stack is empty")
+            
+        return self.items.pop()
+                
+    def top(self):               # 스택의 가장 위에 있는 데이터를 제거하지 않고 반환합니다.
+        if self.empty():
+            raise Exception("Stack is empty")
+                        
+        return self.items[-1]
+
+in_list = list(input())
+s = Stack()
+for i in in_list:
+    if s.empty():
+        s.push(i)
+        if i == ')':  # 비어있는데 )가 들어가면 ()이 나올 수 없음
+            break
+    else:
+        if s.top() != i:
+            s.pop()
+        else:
+            s.push(i)
+
+if s.empty():
+    print('Yes')
+else:
+    print('No')
