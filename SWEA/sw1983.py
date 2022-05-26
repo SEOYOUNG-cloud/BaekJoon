a7477d89a439706560d50ed64a49708b9a0a613a
PARKSEOYOUNG <gksdidtk123@gmail.com>
2022-05-26 15:54:11 +0900

    1983. 조교의 성적 매기기

---
+++ SWEA/sw1983.py
@@ -0,0 +1,21 @@
+score = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
+
+for test_case in range(1, int(input()) + 1):
+    n, k = map(int, input().split())
+    students = []
+    for i in range(1, n+1):
+        student = list(map(int, input().split()))
+        students.append(round(student[0] * 0.35 + student[1] * 0.45 + student[2] * 0.20, 2))
+
+        if i == k:
+            K_score = round(student[0] * 0.35 + student[1] * 0.45 + student[2] * 0.20, 2)
+
+    students.sort(reverse=True)
+    for i in range(n):
+        if students[i] == K_score:
+            K_idx = students.index(K_score)
+
+    print(f'#{test_case} {score[K_idx // (n//10)]}')
+
+
+
