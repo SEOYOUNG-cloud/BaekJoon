da58a98fb39c9a68775cc9a42c8cfdd6c3f17f30
박서영 <gksdidtk123@gmail.com>
2022-09-09 23:15:55 +0900

    콜라스추측.java
    콜라스추측.java

---
+++ programmers/콜라스추측.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n = (long)num;
+        
+        while(n != 1){
+            if(answer == 500){
+                return -1;
+            }
+            if(n % 2 == 0) // 짝수라면
+                n /= 2;
+            else
+                n = n*3 + 1;
+            
+            answer+=1;
+        }
+        return answer;
+    }
+}
