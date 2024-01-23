31dbd7d8510af0540ca3df499a6cc82470d3e5be
박서영 <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2024-01-23 17:30:25 +0900

    올바른 괄호.java

---
+++ programmers/Solution_올바른괄호.java
@@ -0,0 +1,20 @@
+package programmers;
+
+import java.util.*;
+import java.io.*;
+
+public class Solution_올바른괄호 {
+
+    boolean solution(String s) {
+        char[] array = s.toCharArray();
+
+        int n = 0;
+        for(char arr : array){
+            n += arr == '(' ? 1 : -1;
+            if(n < 0) return false;
+        }
+
+        if(n == 0) return true;
+        else return false;
+    }
+}
